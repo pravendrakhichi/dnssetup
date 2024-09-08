6e9531b66667b3a058cba5921ec65549c048165b
Pravendra S Khichi <30142546+pravendrakhichi@users.noreply.github.com>
2024-09-09 02:32:16 +0530

    Update setupjavaworkspace.bat

--- setupjavaworkspace.bat
+++ setupjavaworkspace.bat
@@ -1,10 +1,11 @@
 @echo off
+setlocal
 powershell -command "Invoke-WebRequest -Uri 'https://builds.openlogic.com/downloadJDK/openlogic-openjdk/21.0.4+7/openlogic-openjdk-21.0.4+7-windows-x64.zip' -OutFile 'jdk21.zip'"
 powershell -command "Expand-Archive -Path 'jdk21.zip' -DestinationPath 'java'"
 cd java
-for /d %i in (*) do ren %i "jdk21" 
+for /d %%i in (*) do ren %%i "jdk21" 
 cd ..
-for %%A in ("~dp0.\java\jdk21") Do set "_JAVA21=%%~fA"
+for %%A in ("%~dp0.\java\jdk21") Do set "_JAVA21=%%~fA"
 set JAVA_HOME=%_JAVA21%
 if "%~1" == "java-21" set JAVA_HOME=%_JAVA21%
 if "%~2" == "perm" (
@@ -17,10 +18,10 @@ echo --setting up maven
 powershell -command "Invoke-WebRequest -Uri 'https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.zip' -OutFile 'maven.zip'"
 powershell -command "Expand-Archive -Path 'maven.zip' -DestinationPath 'maven'"
 cd maven
-for /d %i in (*) do ren %i "maven3_9_9" 
+for /d %%i in (*) do ren %%i "maven3_9_9" 
 cd ..
 
-for %A in ("~dp0.\java\jdk17") Do set "_MAVEN=%~fA"
+for %%A in ("%~dp0.\maven\maven3_9_9") Do set "_MAVEN=%%~fA"
 set M2_HOME=%_MAVEN%
 set M2=%_MAVEN%\bin
 if "%~1" == "maven" set M2_HOME=%_MAVEN%
